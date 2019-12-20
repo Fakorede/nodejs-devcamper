ccaea4127ddd7d7e6d4caa47b924471346e4ac4c
fakorede <abiolafakorede@gmail.com>
2019-12-20 15:21:20 +0100

    connected to the db with mongoose

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
+
+// load env variables
+dotenv.config({ path: "./config/config.env" });
+
+// connect to db
+connectDB();
 
 // middleware
 // const logger = require("./middleware/logger");
 
 // route files
 const bootcamps = require("./routes/bootcamps");
-
-// load env variables
-dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
@@ -24,6 +29,14 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Sever running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(
+    `Sever running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  );
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  server.close(() => process.exit(1));
 });
