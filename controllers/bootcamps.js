c4cab3ff0b6f562f1bde440a57bc19168309e742
fakorede <abiolafakorede@gmail.com>
2019-12-23 23:16:32 +0100

    advanced results middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,70 +8,7 @@ const Bootcamp = require("../models/Bootcamp");
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  let query;
-  const reqQuery = { ...req.query };
-
-  // fields to exclude(we dont want to match field in document matching these)
-  const removeFields = ["select", "sort", "page", "limit"];
-
-  removeFields.forEach(param => delete reqQuery[param]);
-
-  let queryStr = JSON.stringify(reqQuery);
-
-  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
-
-  query = Bootcamp.find(JSON.parse(queryStr)).populate("courses");
-
-  // select fields
-  if (req.query.select) {
-    const fields = req.query.select.split(",").join(" ");
-    query = query.select(fields);
-  }
-
-  // sort
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(",").join(" ");
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort("-createdAt");
-  }
-
-  // pagination
-  const page = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.limit, 10) || 25;
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-  const total = await Bootcamp.countDocuments();
-
-  query = query.skip(startIndex).limit(limit);
-
-  // execute query
-  const bootcamps = await query;
-
-  //
-  const pagination = {};
-
-  if (endIndex < total) {
-    pagination.next = {
-      page: page + 1,
-      limit
-    };
-  }
-
-  if (startIndex > 0) {
-    pagination.prev = {
-      page: page - 1,
-      limit
-    };
-  }
-
-  res.status(200).json({
-    success: true,
-    count: bootcamps.length,
-    pagination,
-    message: "Show all bootcamps",
-    data: bootcamps
-  });
+  res.status(200).json(res.advancedResults);
 });
 
 // @desc    Get single bootcamp
