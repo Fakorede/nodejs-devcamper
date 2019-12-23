9a8175d78e88a88bd026cb68b5db771b9b2b9f51
fakorede <abiolafakorede@gmail.com>
2019-12-24 00:00:00 +0100

    register user & encrypt password

--- models/User.js
+++ models/User.js
@@ -36,4 +36,14 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
+// Encrypt password using bcrypt
+UserSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 module.exports = mongoose.model("User", UserSchema);
