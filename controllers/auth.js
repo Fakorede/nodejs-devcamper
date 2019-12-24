6caa2f25a495ba42191298afa7ac686dfbfa7161
fakorede <abiolafakorede@gmail.com>
2019-12-24 15:28:55 +0100

    user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,8 +30,33 @@ exports.register = asyncHandler(async (req, res, next) => {
 // @route     POST /api/v1/auth/login
 // @access    Public
 exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // validate email&password
+  if (!email || !password) {
+    return next(new ErrorResponse(`Please provide an email and password`, 400));
+  }
+
+  // check for user
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorResponse(`User not found`, 401));
+  }
+
+  // check if password match
+  const isMatch = await user.matchPassword(password);
+
+  if (!user) {
+    return next(new ErrorResponse(`Invalid credentials`, 401));
+  }
+
+  // create token
+  const token = user.getSignedJwtToken();
+
   res.status(200).json({
     success: true,
+    token,
     message: "Login User"
   });
 });
