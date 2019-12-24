1c530245540e7480e7db0d7587f58fa2be787b1f
fakorede <abiolafakorede@gmail.com>
2019-12-24 14:51:29 +0100

    sign and get json web token

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,8 +16,12 @@ exports.register = asyncHandler(async (req, res, next) => {
     role
   });
 
+  // Create token
+  const token = user.getSignedJwtToken();
+
   res.status(200).json({
     success: true,
+    token,
     message: "Register User"
   });
 });
